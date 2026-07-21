723c2b855d77d0e2fc66ff27d509aba77748ce02
noah5000 <noahfrost56@gmail.com>
2026-07-21 12:32:37 +0530

    Update and rename lineage_devon.mk to infinity_devon.mk

--- infinity_devon.mk
+++ infinity_devon.mk
@@ -7,6 +7,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+
+
 # Inherit from device
 $(call inherit-product, device/motorola/devon/device.mk)
 
