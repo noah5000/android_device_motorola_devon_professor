7730a3b00ab4791020173bdd3167342cdd39e5f5
noah5000 <noahfrost56@gmail.com>
2026-07-21 12:50:44 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,11 +2,10 @@
 # SPDX-FileCopyrightText: The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
-
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/infinity_devon.mk
+    $(LOCAL_DIR)/lineage_devon.mk
 
 COMMON_LUNCH_CHOICES := \
-    infinity_devon-user \
-    infinity_devon-userdebug \
-    infinity_devon-eng
+    lineage_devon-bp4a-user \
+    lineage_devon-bp4a-userdebug \
+    lineage_devon-bp4a-eng
