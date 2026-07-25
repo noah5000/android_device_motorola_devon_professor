68635dff34f7adf6c02b810134e9ed4d6130bac1
noah5000 <noahfrost56@gmail.com>
2026-07-25 09:26:07 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -7,6 +7,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_devon.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_devon-user \
-    lineage_devon-userdebug \
-    lineage_devon-eng
+    lineage_devon-bp4a-user \
+    lineage_devon-bp4a-userdebug \
+    lineage_devon-bp4a-eng
