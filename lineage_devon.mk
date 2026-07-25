e0864aeacdc257a57e1f7f8296700980e2c967f9
noah5000 <noahfrost56@gmail.com>
2026-07-25 09:40:26 +0530

    Update lineage_devon.mk

--- lineage_devon.mk
+++ lineage_devon.mk
@@ -10,7 +10,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device
 $(call inherit-product, device/motorola/devon/device.mk)
 
-# Inherit some common LineageOS stuff.
+# Inherit some common AxionOS stuff.
+TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
@@ -26,3 +27,18 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="devon_g-user 13 T2SNS33.73-22-3-19 565799-307aa release-keys" \
     BuildFingerprint=motorola/devon_g/devon:13/T2SNS33.73-22-3-19/565799-307aa:user/release-keys \
     DeviceProduct=devon_g
+
+AXION_MAINTAINER := noah5000
+AXION_PROCESSOR := Snapdragon_680
+AXION_CAMERA_REAR_INFO := 50,8
+AXION_CAMERA_FRONT_INFO := 16
+TARGET_INCLUDE_AXFX := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDES_LOS_PREBUILTS := false
+TARGET_DISABLES_LIBPERF := true
+HBM_SUPPORTED := false
+TARGET_NEEDS_DOZE_FIX := false
+TARGET_DOZE_TAP_PULSE_SUPPORTED := false
+TARGET_DOZE_DOUBLE_TAP_PULSE_SUPPORTED := false
+TARGET_DOZE_PICKUP_PULSE_SUPPORTED := false
+TARGET_DOZE_SIDE_FPS_PULSE_SUPPORTED := false
