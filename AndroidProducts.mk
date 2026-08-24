24d7a72603799a500b493d84a4d9929efc648597
noah5000 <noahfrost56@gmail.com>
2026-08-25 00:15:11 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -7,6 +7,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/alpha_devon.mk
 
 COMMON_LUNCH_CHOICES := \
-    infinity_devon-bp4a-user \
-    infinity_devon-bp4a-userdebug \
-    infinity_devon-bp4a-eng
+    alpha_devon-bp4a-user \
+    alpha_devon-bp4a-userdebug \
+    alpha_devon-bp4a-eng
